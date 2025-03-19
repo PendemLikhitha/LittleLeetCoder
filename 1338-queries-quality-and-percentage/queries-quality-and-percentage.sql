c27c569c8b9b0050a1d2ec66164d26a5ee99144c
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-03-19 19:03:41 +0530

    Time: 1126 ms (7.85%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query satement below
 select query_name,
-ROUND(avg(cast(rating as decimal)/position),2) AS quality,
+ROUND(avg(rating/position),2) AS quality,
 ROUND(sum(case when rating<3 then 1 else 0 end)*100/count(*),2) AS poor_query_percentage
  FROM Queries
  GROUP BY query_name;
