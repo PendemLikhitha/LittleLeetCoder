5cf17570939ca03f0aee217c867db57c51746cf9
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-03-22 20:09:04 +0530

    Time: 559 ms (95.84%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,17 +1,8 @@
-SELECT
-  ROUND(
-    COUNT(A1.player_id)
-    / (SELECT COUNT(DISTINCT A3.player_id) FROM Activity A3)
-  , 2) AS fraction
-FROM
-  Activity A1
-WHERE
-  (A1.player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN (
-    SELECT
-      A2.player_id,
-      MIN(A2.event_date)
-    FROM
-      Activity A2
-    GROUP BY
-      A2.player_id
-  );
+SELECT ROUND(COUNT(DISTINCT player_id)/
+       (SELECT COUNT(DISTINCT player_id) FROM activity),2) AS fraction
+FROM activity
+WHERE (player_id,event_date) IN (
+        SELECT player_id, ADDDATE(MIN(event_date),1)
+        FROM activity
+        GROUP BY player_id
+    )
