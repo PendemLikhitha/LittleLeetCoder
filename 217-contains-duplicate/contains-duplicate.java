240ad0cf94a81c7a8435324fef4f48dc3c4d445d
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-06-08 19:06:54 +0530

    Time: 13 ms (82.13%) | Memory: 58.5 MB (40.04%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                return true;
-            }
+       HashSet<Integer> seen = new HashSet<>();
+       for(int i:nums){
+        if(seen.contains(i)){
+            return true;
         }
-        return false;
-        
+        seen.add(i);
+       }
+       return false;
     }
 }
