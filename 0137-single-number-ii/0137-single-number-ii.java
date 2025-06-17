c68f37aca313e41a00a03599e27c86cc0ded3b80
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-06-17 19:33:32 +0530

    Time: 0 ms (100%), Space: 45.2 MB (90.72%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,14 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> h = new HashMap<>();
-        for(int i:nums){
-            h.put(i,h.getOrDefault(i,0)+1);
-        }
-        for(int i:h.keySet()){
-            if(h.get(i)==1){
-                return i;
-            }
-        }
-        return -1;
+       int ones = 0;
+       int twos = 0;
+       for(int i:nums){
+        ones = (ones^i) & ~twos;
+        twos = (twos^i) & ~ones;
+
+       }
+       return ones;
     }
 }
