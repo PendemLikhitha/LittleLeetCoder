9b744aad3f7746baf4435520462236e02daf97b5
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-07-24 07:35:35 +0530

    Time: 1 ms (55.91%), Space: 41.8 MB (16.31%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int count = 0;
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                stack.push(c);
+            }
+            else if(c == ')'){
+                if(stack.isEmpty()){
+                    count++;
+                }
+                else{
+                    stack.pop();
+                }
+            }
+        }
+        return count + stack.size();
+    }
+}
