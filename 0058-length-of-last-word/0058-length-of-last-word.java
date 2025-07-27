21ca03b2813bedd40245247a35c7ba5ef5ba24b6
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-07-27 20:19:25 +0530

    Time: 1 ms (32.16%), Space: 41.7 MB (81.56%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int n = s.length()-1;
-        while(n>=0 && s.charAt(n)==' '){
-            n--;
-        }
-        int lastIndex = n;
-        while(n>=0 && s.charAt(n)!=' '){
-            n--;
-        }
-        return lastIndex - n;
+         String res[] = s.split(" ");
+        int n = res.length;
+        return res[n-1].length();
     }
 }
