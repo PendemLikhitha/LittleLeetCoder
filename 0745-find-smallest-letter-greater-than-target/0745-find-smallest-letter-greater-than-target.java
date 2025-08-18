7d3a5e050f3f6b25bad87731cfa6f203a9b20a00
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-08-18 07:49:49 +0530

    Time: 0 ms (100%), Space: 44.7 MB (26.43%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
@@ -5,13 +5,12 @@ class Solution {
         while(low <= high) {
             int mid = (low + high) / 2;
             if(letters[mid] > target) {
-                pos = mid;
                 high = mid - 1;
             }
             else{
                 low = mid + 1;
             }
         } 
-        return pos == -1 ? letters[0]:letters[pos];
+        return letters[low % letters.length];
     }
 }
