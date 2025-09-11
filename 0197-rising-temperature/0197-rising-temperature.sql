cb9cfb5950d7dda48e6c40bc3d490399ea29924c
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2025-09-11 15:39:32 +0530

    Time: 557 ms (20.14%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,4 +2,5 @@
 SELECT w1.id as id
 FROM 
     Weather w1 JOIN Weather w2
-    ON w1.temperature > w2.temperature AND DATEDIFF(w1.recordDate,w2.recordDate) = 1;
+    ON DATEDIFF(w1.recordDate,w2.recordDate) = 1
+WHERE w1.temperature> w2.temperature;
