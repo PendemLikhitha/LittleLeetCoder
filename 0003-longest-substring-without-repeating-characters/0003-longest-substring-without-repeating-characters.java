94513d38d960fc13b8014636882215c5817ea073
PendemLikhitha <159911587+PendemLikhitha@users.noreply.github.com>
2026-05-03 13:04:01 +0530

    Time: 5 ms (87.04%), Space: 44.8 MB (91.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,11 +4,11 @@ class Solution {
         int left = 0;
         HashMap<Character, Integer> m = new HashMap<>();
         for(int right = 0; right<s.length();right++){
-            m.put(s.charAt(right),m.getOrDefault(s.charAt(right),0)+1);
-            while(m.get(s.charAt(right))>1){
-                 m.put(s.charAt(left),m.getOrDefault(s.charAt(left),0)-1);
-                 left++;
+            char ch = s.charAt(right);
+            if(m.containsKey(ch)){
+                left = Math.max(left, m.get(ch)+1);
             }
+            m.put(ch,right);
             longest= Math.max(right - left + 1,longest);
         }
         return longest;
